301d49e5b8daeec7a5438d5e5fcbfd13df05fa23
Abbas4tech <shaikhabbas54@gmail.com>
2023-07-05 22:40:12 +0530

    💫 update: update menifest, favicon and title

--- src/app/modules/movies/screens/detailed/detail.screen.ts
+++ src/app/modules/movies/screens/detailed/detail.screen.ts
@@ -1,21 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { Movie } from '../../model/movies.model';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'movie-detailed-screen',
   templateUrl: './detail.screen.html',
   styleUrls: ['./detail.screen.scss'],
 })
-export class MovieDetailScreen {
+export class MovieDetailScreen implements OnInit {
   movie!: Movie;
   private readonly _ActivatedRoute = inject(ActivatedRoute);
-  constructor() {
+  constructor(private _title: Title) {
     this._ActivatedRoute.data
       .pipe(map(({ data }) => data as Movie))
       .subscribe((movie) => {
         this.movie = movie;
       });
+  }
+  ngOnInit(): void {
+    this._title.setTitle(`${this.movie.Title} | NetflixJr`);
   }
 }
